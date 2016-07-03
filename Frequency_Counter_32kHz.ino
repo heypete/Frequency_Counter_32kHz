cde6f093aa2d6b71e4b48e3e70504524eec6aa6f
heypete <pete@heypete.com>
2016-07-04 01:57:19 +0200

    Re-worked some functions. Improved comments.

--- Frequency_Counter_32kHz.ino
+++ Frequency_Counter_32kHz.ino
@@ -7,34 +7,42 @@ unsigned int printDifference = 0;
 unsigned int printNumSeconds = 0;
 unsigned int printTimerOverflows = 0;
 unsigned long printTicks = 0 - 1;
+unsigned int oldNumSeconds = 65536;
+unsigned long oldTicks = 0;
 boolean firstRun = 1;
 
 void startstop() {
-  /* This is called by the interrupt. It needs to be fast.
-   */
-  ticks = TCNT1;
-  numSeconds++;
+  /* This is called by the PPS interrupt. It needs to be fast. */
+  ticks = TCNT1; // Read TIMER1 and save its current value to a global variable.
+  numSeconds++; // Increment the number of seconds as marked by the PPS signal.
 
 }
 
+/* TIMER1 is a 16-bit counter. Each tick of the 32kHz line increments the counter by one.
+ * This interrupt fires when the timer overflows and rolls over back to zero, incrementing
+ * the timerOverflows variable by one. This is also very fast.
+*/
 ISR(TIMER1_OVF_vect) {
   timerOverflows++;
 }
 
-void setup() {
+void timer1_init() {
+  /* The Arduino core enables the waveform generator, which we don't need or want since it conflicts with using TIMER1 for counting,
+   * so we must disable the waveform generator.
+   */
+  TCCR1A &= ~((1 << WGM12) | (1 << WGM11) | (1 << WGM10)); // Turn off the waveform generator.
 
-  /* The Arduino core enables the waveform generator. This conflicts with using TIMER1 for counting,
-  * so we must disable the waveform generator.
-  */
-  TCCR1A &= ~((1 << WGM12) | (1 << WGM11) | (1 << WGM10));
+  /* Setting CS12 and CS11 to 1 sets TIMER1 to use an external clock source and trigger on the falling edge.
+   * Why the falling edge? With a 10k pull-up resistor on the DS3231's 32kHz pin, I found the falling edge to be much sharper.
+   * Your mileage may vary.
+   */
+  TCCR1B |= ((1 << CS12) | (1 << CS11)); // Trigger on falling edge.
+  // TCCR1B |= ((1 << CS12) | (1 << CS11) | (1 << CS10)); // Trigger on rising edge.
 
-  /* Setting CS12 and CS11 to 1 sets TIMER1 to use an external clock source and trigger on the falling edge. */
-  TCCR1B |= ((1 << CS12) | (1 << CS11));
-
-  /* Initialize the counter. */
+  /* Initialize TIMER1's counter. */
   TCNT1 = 0;
 
-  /* Initialize the TIMER1 overflow interrupt. We should never need this, but better safe than sorry. */
+  /* Initialize the TIMER1 overflow interrupt. */
   TIMSK1 |= (1 << TOIE1);
 
   // Initalize the TIMER1 overflow counter.
@@ -43,7 +51,66 @@ void setup() {
   /* Enable pulse-per-second interrupt. */
   attachInterrupt(digitalPinToInterrupt(2), startstop, RISING);
 
+  /* Enable global interrupts. */
   sei();
+}
+
+void resetCounters() {
+
+  /* To prevent mis-counting, we need to wait for the first full second, then reset the counters.  */
+  Serial.println("Initializing counters...");
+  numSeconds = 0;
+  while (numSeconds < 2) {
+  }
+  /* Resetting the counters must be atomic, so we disable interrupts first, reset the counters, then re-enable interrupts.*/
+  cli(); // Disable interrupts.
+  firstRun = 0; // We only need to run this once. After it's run once, turn it off.
+  numSeconds = 0; // Reset seconds counter.
+  timerOverflows = 0; // Reset the number of times TIMER1 has overflowed.
+  ticks = 0; // Reset the number of 32kHz ticks.
+  printTicks = 0; // Reset the tick counter we use to print results over serial.
+  TCNT1 = 0; // Reset TIMER1.
+  sei(); // Re-enable interrupts.
+
+}
+
+void printResults() {
+  /* TIMER1 overflows every 65536 ticks. Each time it does it fires an interrupt. We multiply the number of times the timer has overflowed
+  by 65536 ticks, then add the current value of the timer. This yields the total number of ticks since the timer was reset. */
+  printTicks += timerOverflows * 65536;
+
+  /* Print the results. */
+  Serial.print(printNumSeconds);
+  Serial.print(" second(s), ");
+  Serial.print(printTicks - oldTicks);
+  Serial.print(" ticks this second, ");
+  Serial.print(printTicks);
+  Serial.print(" total ticks, ");
+  Serial.print(timerOverflows);
+  Serial.print(" timer overflows, ");
+
+  /* An ideal 32kHz oscillator ticks 32768 times per second. Here we calculate the difference between the number of actual ticks counted
+  in a given time period compared with the ideal number over the same time period and convert it to parts per million. */
+  float result = 0;
+  result = (1.0 - ((float)printNumSeconds * 32768.0 / (float)printTicks)) * 1000000.0;
+  Serial.print(result);
+  Serial.println(" ppm");
+
+  /* Update the "old" values so we can compare them the next time through. */
+  oldNumSeconds = printNumSeconds;
+  oldTicks = printTicks;
+
+  /* Are we done? If so, stop. */
+  if (printNumSeconds >= maxSeconds) {
+    Serial.println("Done. Reset to start again.");
+    while (1) {};
+  }
+}
+
+
+void setup() {
+
+  timer1_init();
 
   /* Setup serial to run at 9600 bps, no parity, one stop bit. */
   Serial.begin(9600);
@@ -56,65 +123,31 @@ void setup() {
   /* Confirm the number of seconds to count for. */
   Serial.print("Counting for ");
   Serial.print(maxSeconds);
-  Serial.println(" seconds");
+  Serial.println(" seconds.");
 
-  /* To prevent mis-counting, we need to wait for the first full second, then reset the counters.  */
-  Serial.print("Waiting for first full second...");
-  while (numSeconds < 2) {
-  }
-  Serial.println("done.");
-  Serial.println("Starting...");
-
-  /* Resetting the counters must be atomic, so we disable interrupts first, reset the counters, then re-enable interrupts.*/
-  cli();
-  TCNT1 = 0;
-  numSeconds = 0;
-  timerOverflows = 0;
-  ticks = 0;
-  printTicks = 0;
-  sei();
 }
 
 
 void loop() {
-  if (firstRun) {
-    while (numSeconds < 2) {
-    }
-    cli();
-    TCNT1 = 0;
-    numSeconds = 0;
-    timerOverflows = 0;
-    ticks = 0;
-    printTicks = 0;
-    sei();
-    firstRun = 0;
+  if (firstRun) { // If this is the first time through the loop, reset everything to start from a consistent state.
+    resetCounters();
   }
-  static unsigned int oldNumSeconds = 65536;
-  static unsigned long oldTicks = 0;
-  cli();
-  unsigned int printNumSeconds = numSeconds;
-  unsigned long printTicks = ticks;
-  sei();
-  if (printNumSeconds > oldNumSeconds) {
-    printTicks += timerOverflows*65536;
-    Serial.print(printNumSeconds);
-    Serial.print(" seconds, ");
-    Serial.print(printTicks - oldTicks);
-    Serial.print(" ticks this second, ");
-    Serial.print(printTicks);
-    Serial.print(" total ticks, ");
-    Serial.print(timerOverflows);
-    Serial.print(" timer overflows, ");
-    float result = 0;
-    result = (1.0 - ((float)printNumSeconds * 32768.0 / (float)printTicks)) * 1000000.0;
-    Serial.print(result);
-    Serial.println(" ppm");
-    oldNumSeconds = printNumSeconds;
-    oldTicks = printTicks;
-    /* Are we done? If so, stop. */
-    if (printNumSeconds >= maxSeconds) {
-      Serial.println("Done. Reset to start again.");
-      while (1) {};
-    }
+
+  /* Interrupts can occur at any time and can change the "ticks" and "numSeconds" variables without warning, causing corruption.
+   * We can avoid this by briefly disabling interrupts, copying the values into other variables not changed by interrupts,
+   * then re-enabling interrupts. This is very fast and only takes a few clock cycles.
+  */
+  cli(); // Disable interrupts.
+  printNumSeconds = numSeconds; // Copy value into new variable.
+  printTicks = ticks; // Copy value into new variable.
+  sei(); // Re-enable interrupts.
+
+  /* Wait until we have new data, then print the results to the serial console.
+   * We could use delay(1000) instead, but over long periods of time the start of the serial message would drift relative
+   * to the PPS signal marking the start of each second. This drift might cause issues, so it's better to keep the serial
+   * messages more closely aligned to the start of the second.
+   */
+  if (printNumSeconds > oldNumSeconds) { // Check to see if its a new second.
+    printResults();
   }
 }
